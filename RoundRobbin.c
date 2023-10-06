c059d0e17f74e9b09a77066bd9342d800b1a3d9d
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-10-06 07:45:50 +0530

    robbin

--- RoundRobbin.c
+++ RoundRobbin.c
@@ -42,6 +42,18 @@ int main(){
         printf("%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct, tat[i], wt[i]);
     }
 
+    // Calculating average waiting time and average turnaround time
+    float avg_wt=0, avg_tat=0;
+    for(i=0; i<nop; i++){
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+    avg_wt /= nop;
+    avg_tat /= nop;
+    printf("Average waiting time: %f\n", avg_wt);
+    printf("Average turnaround time: %f\n", avg_tat);
+    
+
     return 0;
     
 }
